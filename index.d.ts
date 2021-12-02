242d30c3c55076040a54f7c3cf1f8f570f5aae4d
nev <gh@f-m.fm>
2021-12-02 07:03:20 -0800

    .

---
+++ index.d.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from "bungie-api-ts/http";
+export declare function createOauthHttpClient(apiKey: string, client_id: string, client_secret: string, 
+/**
+ * a function to store arbitrary JSON-encodable data.
+ * this should return an authentication token object.
+ */
+retrieveToken: () => BungieNetTokenMeta | Promise<BungieNetTokenMeta>, 
+/**
+ * a function to retrieve arbitrary JSON-encodable data.
+ * the authentication token object will be sent as a param to this function.
+ */
+storeToken: (_: BungieNetTokenMeta) => void | Promise<void>, options?: {
+    /**
+     * always ON, unless explicitly set to false. this backs off increasingly,
+     * delaying new api requests as previous ones encounter downtime or throttling responses.
+     *
+     * this will not automatically retry, the error is still passed upstack.
+     * this simply decreases chances of encountering repeated errors.
+     */
+    responsiveThrottling?: boolean;
+    /**
+     * if set, this client will abort the request after some time,
+     * then run the onTimeout function to notify upstack of what happened
+     */
+    withAbortTimeout?: {
+        timeout: number;
+        onTimeout?: (startTime: number, timeout: number) => void;
+    };
+    /**
+     * if set, this client will run the onTimeout function if the request is taking a long time,
+     * e.g. generate a "still waiting!" notification
+     */
+    withWarningTimeout?: {
+        timeout: number;
+        onTimeout: (startTime: number, timeout: number) => void;
+    };
+    verbose?: boolean;
+}): HttpClient;
+export interface BungieNetToken {
+    access_token: string;
+    token_type: string;
+    expires_in: number;
+    refresh_token: string;
+    refresh_expires_in: number;
+    membership_id: string;
+}
+export interface BungieNetTokenMeta {
+    token: BungieNetToken;
+    expires_at: number;
+    refresh_expires_at: number;
+}
---
+++ index.js
@@ -0,0 +1,93 @@
+import { createHttpClient } from "httpclient";
+const TOKEN_URL = "https://www.bungie.net/platform/app/oauth/token/";
+export function createOauthHttpClient(apiKey, client_id, client_secret, 
+/**
+ * a function to store arbitrary JSON-encodable data.
+ * this should return an authentication token object.
+ */
+retrieveToken, 
+/**
+ * a function to retrieve arbitrary JSON-encodable data.
+ * the authentication token object will be sent as a param to this function.
+ */
+storeToken, options = {}) {
+    async function fetchWithBungieOAuth(request, requestOptions) {
+        const tokenMeta = await retrieveToken();
+        if (!tokenMeta)
+            throw `failed to retrieve auth token from storage!
+is there a problem with the retrieveToken function?
+or has no initial token been issued yet?`;
+        const { expires_at, refresh_expires_at, token } = tokenMeta;
+        let { refresh_token, access_token } = token;
+        const now = Date.now();
+        // haven't reached expiry yet
+        if (now < expires_at) {
+            // we're good as-is
+            options.verbose && console.info("still active ☑️ ");
+        }
+        // token expired but refresh available
+        else if (now < refresh_expires_at) {
+            const body = new URLSearchParams({
+                grant_type: "refresh_token",
+                refresh_token,
+                client_id,
+                client_secret,
+            });
+            let fetchedNewToken;
+            try {
+                fetchedNewToken = await fetch(TOKEN_URL, {
+                    method: "POST",
+                    body,
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded",
+                    },
+                });
+            }
+            catch (e) {
+                console.log(e);
+                throw "fetch failed ❌";
+            }
+            let parsedNewToken;
+            try {
+                parsedNewToken = await fetchedNewToken.json();
+            }
+            catch (e) {
+                console.log(e);
+                throw "parse failed ❌";
+            }
+            if (!looksLikeBnetAuthToken(parsedNewToken)) {
+                console.error(token);
+                throw `this doesn't look like a token`;
+            }
+            options.verbose && console.info("refreshed ☑️ ");
+            // token refreshed, yay
+            const tokenMeta = {
+                token: parsedNewToken,
+                expires_at: Date.now() + token.expires_in * 1000,
+                refresh_expires_at: Date.now() + token.refresh_expires_in * 1000,
+            };
+            await storeToken(tokenMeta);
+            access_token = parsedNewToken.access_token;
+        }
+        else {
+            throw "auth expired ❌";
+        }
+        if (typeof request === "string")
+            request = new Request(request);
+        request.headers.set("Authorization", `Bearer ${access_token}`);
+        return await fetch(request, requestOptions);
+    }
+    return createHttpClient(apiKey, {
+        ...options,
+        fetchFunctionOverride: fetchWithBungieOAuth,
+    });
+}
+function looksLikeBnetAuthToken(token) {
+    return (token &&
+        typeof token.access_token === "string" &&
+        typeof token.token_type === "string" &&
+        typeof token.expires_in === "number" &&
+        typeof token.refresh_token === "string" &&
+        typeof token.refresh_expires_in === "number" &&
+        typeof token.membership_id === "string");
+}
